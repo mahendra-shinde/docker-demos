b71f0e6642c3a4af8ddb023bd24b44944a79b0be
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2018-09-22 10:44:55 +0530

    modified the web page

--- php-demo/files/index.php
+++ php-demo/files/index.php
@@ -3,6 +3,6 @@
 <head><title>Container demo - PHP</title>
 </head>
 <body>
-<h1>Welcome from <%= gethostbyname() %> </h1>
+<h1>Welcome from container/pod <?php echo gethostname(); ?> </h1>
 </body>
 </html>
